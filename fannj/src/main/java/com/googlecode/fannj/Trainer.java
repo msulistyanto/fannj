6e162adb8c0a8d9e642b176b211c239852f00c63
kylerenfro <kylerenfro@6ca3966c-cae7-11de-891b-638738bb322f>
2009-11-06 15:55:18 +0000

    Initial commit.

---
+++ fannj/src/main/java/com/googlecode/fannj/Trainer.java
@@ -0,0 +1,29 @@
+package com.googlecode.fannj;
+
+import com.sun.jna.Pointer;
+
+public class Trainer {
+    
+    Fann fann;
+    
+    public Trainer( Fann fann ){
+        this.fann = fann;
+    }
+    
+    public float train( 
+            String trainingFile, 
+            int maxEpochs, 
+            int epochsBetweenReports, 
+            float desiredError ){
+        
+        fann_train_on_file( 
+                fann.ann, trainingFile, maxEpochs, epochsBetweenReports, desiredError );
+        return fann_get_MSE( fann.ann );
+    }
+    
+    /* A JNA Direct Mapping implementation of the FANN library.  */
+    protected static native void fann_train_on_file( 
+            Pointer ann, String filename, int max_epochs, int epochs_between_reports, float desired_error );
+    protected static native float fann_get_MSE( Pointer ann );
+
+}
