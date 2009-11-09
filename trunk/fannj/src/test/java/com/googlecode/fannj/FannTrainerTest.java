1ffd2c2c8ebc8f7fbd4a41f374029161d6eda9a3
kylerenfro <kylerenfro@6ca3966c-cae7-11de-891b-638738bb322f>
2009-11-09 15:58:24 +0000

    Removed com.googlecode.fannj.jna package and all classes. Updated code formatting. A

--- trunk/fannj/src/test/java/com/googlecode/fannj/FannTrainerTest.java
+++ trunk/fannj/src/test/java/com/googlecode/fannj/FannTrainerTest.java
@@ -8,26 +8,24 @@ import java.util.List;
 
 import org.junit.Test;
 
-
 public class FannTrainerTest {
 
     @Test
-    public void testTraining(){
-     
-        URL trainingFile = this.getClass().getResource( "xor.data" );
-        
-        List<Layer> layers = new ArrayList<Layer>();
-        layers.add( Layer.create( 2 ));
-        layers.add( Layer.create( 3, ActivationFunction.FANN_SIGMOID_SYMMETRIC ));
-        layers.add( Layer.create( 1, ActivationFunction.FANN_SIGMOID_SYMMETRIC ));
-        
-        Fann fann = new Fann( layers );
-        Trainer trainer = new Trainer( fann );
-        
-        float desiredError = .001f;
-        float mse = trainer.train( 
-                trainingFile.getFile(), 500000, 1000, desiredError );
-        assertTrue( "" + mse, mse <= desiredError );
+    public void testTraining() {
+
+	URL trainingFile = this.getClass().getResource("xor.data");
+
+	List<Layer> layers = new ArrayList<Layer>();
+	layers.add(Layer.create(2));
+	layers.add(Layer.create(3, ActivationFunction.FANN_SIGMOID_SYMMETRIC));
+	layers.add(Layer.create(1, ActivationFunction.FANN_SIGMOID_SYMMETRIC));
+
+	Fann fann = new Fann(layers);
+	Trainer trainer = new Trainer(fann);
+
+	float desiredError = .001f;
+	float mse = trainer.train(trainingFile.getFile(), 500000, 1000, desiredError);
+	assertTrue("" + mse, mse <= desiredError);
     }
-    
+
 }
