55feb8281beebb012e870099acc9dcc3ad70680c
kylerenfro <kylerenfro@6ca3966c-cae7-11de-891b-638738bb322f>
2009-11-09 15:58:24 +0000

    Removed com.googlecode.fannj.jna package and all classes. Updated code formatting. A

--- fannj/src/main/java/com/googlecode/fannj/Trainer.java
+++ fannj/src/main/java/com/googlecode/fannj/Trainer.java
@@ -5,36 +5,33 @@ import com.sun.jna.Platform;
 import com.sun.jna.Pointer;
 
 /**
- * Trains an ANN.
+ * Trains an ANN. Currently only File based training is supported.
  * 
  * @author krenfro
  */
 public class Trainer {
 
-    static{
-        Native.register( Platform.isWindows() ? "fann" : "fann" );
+    static {
+	Native.register(Platform.isWindows() ? "fann" : "fann");
     }
-    
+
     Fann fann;
-    
-    public Trainer( Fann fann ){
-        this.fann = fann;
+
+    public Trainer(Fann fann) {
+	this.fann = fann;
     }
-    
-    public float train( 
-            String trainingFile, 
-            int maxEpochs, 
-            int epochsBetweenReports, 
-            float desiredError ){
-        
-        fann_train_on_file( 
-                fann.ann, trainingFile, maxEpochs, epochsBetweenReports, desiredError );
-        return fann_get_MSE( fann.ann );
+
+    public float train(String trainingFile, int maxEpochs, int epochsBetweenReports,
+	    float desiredError) {
+
+	fann_train_on_file(fann.ann, trainingFile, maxEpochs, epochsBetweenReports, desiredError);
+	return fann_get_MSE(fann.ann);
     }
-    
-    /* A JNA Direct Mapping implementation of the FANN library.  */
-    protected static native void fann_train_on_file( 
-            Pointer ann, String filename, int max_epochs, int epochs_between_reports, float desired_error );
-    protected static native float fann_get_MSE( Pointer ann );
+
+    /* A JNA Direct Mapping implementation of the FANN library. */
+    protected static native void fann_train_on_file(Pointer ann, String filename, int max_epochs,
+	    int epochs_between_reports, float desired_error);
+
+    protected static native float fann_get_MSE(Pointer ann);
 
 }
--- fannj/src/test/java/com/googlecode/fannj/FannTest.java
+++ fannj/src/test/java/com/googlecode/fannj/FannTest.java
@@ -10,21 +10,18 @@ import com.googlecode.fannj.Fann;
 
 public class FannTest {
 
-    
     @Test
-    public void testFromFile(){
+    public void testFromFile() {
 
-        URL fannFile = this.getClass().getResource( "xor_float.net" );
-        Fann fann = new Fann( fannFile.getFile() );        
-        assertEquals( 2, fann.getNumInputNeurons() );
-        assertEquals( 1, fann.getNumOutputNeurons() );        
-        assertEquals( -1f, fann.run( new float[]{ -1, -1 })[0], .2f );
-        assertEquals( 1f, fann.run( new float[]{ -1, 1 })[0], .2f );
-        assertEquals( 1f, fann.run( new float[]{ 1, -1 })[0], .2f );
-        assertEquals( -1f, fann.run( new float[]{ 1, 1 })[0], .2f );        
-        fann.close();
+	URL fannFile = this.getClass().getResource("xor_float.net");
+	Fann fann = new Fann(fannFile.getFile());
+	assertEquals(2, fann.getNumInputNeurons());
+	assertEquals(1, fann.getNumOutputNeurons());
+	assertEquals(-1f, fann.run(new float[] { -1, -1 })[0], .2f);
+	assertEquals(1f, fann.run(new float[] { -1, 1 })[0], .2f);
+	assertEquals(1f, fann.run(new float[] { 1, -1 })[0], .2f);
+	assertEquals(-1f, fann.run(new float[] { 1, 1 })[0], .2f);
+	fann.close();
     }
-    
-    
 
 }
