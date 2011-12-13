0940a287e551cf62dca9c56871a0af467b045c1a
kylerenfro <kylerenfro@gmail.com>
2011-12-13 23:06:44 +0000

    Updated tests to use common-io to copy test resources files to temporary files.  This is required for the tests to work on Windows.

--- fannj/src/test/java/com/googlecode/fannj/FannTrainerTest.java
+++ fannj/src/test/java/com/googlecode/fannj/FannTrainerTest.java
@@ -17,20 +17,27 @@
  */
 package com.googlecode.fannj;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import static org.junit.Assert.assertTrue;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 
 public class FannTrainerTest {
 
     @Test
-    public void testTrainingDefault() {
-
-        URL trainingFile = this.getClass().getResource("xor.data");
+    public void testTrainingDefault() throws IOException {
+ 
+        File temp = File.createTempFile("fannj_", ".tmp");
+        temp.deleteOnExit();
+        IOUtils.copy(this.getClass().getResourceAsStream("xor.data"), 
+                new FileOutputStream(temp));
 
         List<Layer> layers = new ArrayList<Layer>();
         layers.add(Layer.create(2));
@@ -41,16 +48,20 @@ public class FannTrainerTest {
         Trainer trainer = new Trainer(fann);
 
         float desiredError = .001f;
-        float mse = trainer.train(trainingFile.getFile(), 500000, 1000,
+        float mse = trainer.train(temp.getPath(), 500000, 1000,
                 desiredError);
         assertTrue("" + mse, mse <= desiredError);
     }
 
     @Test
-    public void testTrainingQuickprop() {
+    public void testTrainingQuickprop() throws IOException {
 
-        URL trainingFile = this.getClass().getResource("xor.data");
-
+        File temp = File.createTempFile("fannj_", ".tmp");
+        temp.deleteOnExit();
+        IOUtils.copy(this.getClass().getResourceAsStream("xor.data"), 
+                new FileOutputStream(temp));
+        
+        
         List<Layer> layers = new ArrayList<Layer>();
         layers.add(Layer.create(2));
         layers.add(Layer.create(3, ActivationFunction.FANN_SIGMOID_SYMMETRIC));
@@ -62,16 +73,20 @@ public class FannTrainerTest {
         trainer.setTrainingAlgorithm(TrainingAlgorithm.FANN_TRAIN_QUICKPROP);
 
         float desiredError = .001f;
-        float mse = trainer.train(trainingFile.getFile(), 500000, 1000,
+        float mse = trainer.train(temp.getPath(), 500000, 1000,
                 desiredError);
         assertTrue("" + mse, mse <= desiredError);
     }
 
     @Test
-    public void testTrainingBackprop() {
+    public void testTrainingBackprop() throws IOException {
 
-        URL trainingFile = this.getClass().getResource("xor.data");
-
+        
+        File temp = File.createTempFile("fannj_", ".tmp");
+        temp.deleteOnExit();
+        IOUtils.copy(this.getClass().getResourceAsStream("xor.data"), 
+                new FileOutputStream(temp));
+        
         List<Layer> layers = new ArrayList<Layer>();
         layers.add(Layer.create(2));
         layers.add(Layer.create(3, ActivationFunction.FANN_SIGMOID_SYMMETRIC));
@@ -84,19 +99,25 @@ public class FannTrainerTest {
         trainer.setTrainingAlgorithm(TrainingAlgorithm.FANN_TRAIN_INCREMENTAL);
 
         float desiredError = .001f;
-        float mse = trainer.train(trainingFile.getFile(), 500000, 1000,
+        float mse = trainer.train(temp.getPath(), 500000, 1000,
                 desiredError);
         assertTrue("" + mse, mse <= desiredError);
     }
 
     @Test
-    public void testCascadeTraining() {
-        URL trainingFile = this.getClass().getResource("parity8.train");
+    public void testCascadeTraining() throws IOException {
+        
+        
+        File temp = File.createTempFile("fannj_", ".tmp");
+        temp.deleteOnExit();
+        IOUtils.copy(this.getClass().getResourceAsStream("parity8.train"), 
+                new FileOutputStream(temp));
+        
         Fann fann = new FannShortcut(8, 1);
         Trainer trainer = new Trainer(fann);
 
         float desiredError = .00f;
-        float mse = trainer.cascadeTrain(trainingFile.getFile(), 30, 1,
+        float mse = trainer.cascadeTrain(temp.getPath(), 30, 1,
                 desiredError);
 
         assertTrue("" + mse, mse <= desiredError);
